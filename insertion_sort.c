2b1bb84b608bed2d346378f52dfdc279d52102ec
Ivan22339900 <162419311+Ivan22339900@users.noreply.github.com>
2024-03-20 00:59:47 +0300

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -23,8 +23,15 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    printf("Podaj liczbę elementów w tablicy: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Podaj elementy tablicy: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
     insertionSort(arr, n);
     printf("Posortowana tablica: \n");
